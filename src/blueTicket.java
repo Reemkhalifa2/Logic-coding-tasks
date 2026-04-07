d42dddf212a5c53100eae7d2bc352d78856dcb0c
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-07 11:51:03 +0400

    add task10 : blueTicket

---
+++ src/blueTicket.java
@@ -0,0 +1,12 @@
+public class blueTicket {
+    public Integer blueTicket(Integer a, Integer b, Integer c){
+        if (a + b == 10 || a + c == 10 || b+c == 10){
+            return 10;
+        } else if((a + b == (b + c) + 10) || (a + b == (a + c) + 10)) {
+            return 5;
+        }else {
+            return 0;
+
+        }
+    }
+}
