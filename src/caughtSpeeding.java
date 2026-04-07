62fc8a46f7233aad74a331190d24e3ca817e95bd
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-07 10:44:34 +0400

    add task2 : caughtSpeeding

---
+++ src/caughtSpeeding.java
@@ -0,0 +1,21 @@
+public class caughtSpeeding {
+    public Integer caughtSpeeding(Integer speed, Boolean isBirthday) {
+        if (isBirthday) {
+            if (speed <= 65) {
+                return 0;
+            } else if (speed <= 85) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            if (speed <= 60) {
+                return 0;
+            } else if (speed <= 80) {
+                return 1;
+            } else {
+                return 2;
+            }
+        }
+    }
+}
