a938dc6486f3106948b22c30476bf60abe444d75
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-07 16:08:06 +0400

    add taskl

---
+++ src/fizzString2.java
@@ -0,0 +1,18 @@
+public class fizzString2 {
+    public String fizzString2(Integer n){
+        if (n%5==0 && n%3==0){
+            return "FizzBuzz!";
+        }
+        else if (n%5==0){
+            return "Buzz!";
+        }
+
+        else if (n%3==0){
+            return "Fizz!";
+        }else{
+            return n + "!";
+        }
+
+    }
+
+}
