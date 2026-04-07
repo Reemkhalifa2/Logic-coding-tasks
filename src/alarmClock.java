4102e45e62a03bc6061ba6159b3c2dfd21c5d5a2
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-07 15:29:59 +0400

    add task22 : alarmClock

---
+++ src/alarmClock.java
@@ -0,0 +1,10 @@
+public class alarmClock {
+    public String alarmClock(int day, boolean vacation){
+        if(day >= 1 && day <= 5){
+            return vacation ? "10:00" : "7:00";
+        }else{
+            return vacation ? "off" : "10:00";
+
+        }
+    }
+}
