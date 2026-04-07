9a187c651ff995758bb32e17865e0e183d7d8359
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-07 15:05:43 +0400

    add task16 : fizzString

--- src/fizzString.java
+++ src/fizzString.java
@@ -1,10 +1,10 @@
 public class fizzString {
     public String fizzString(String str){
-        if((str.charAt(0) == 'f') && (str.charAt(str.length() - 1) == 'b')){
+        if((str.startsWith("f")) && (str.endsWith("b"))){
             return "FizzBuzz";
-        } else if (str.charAt(0) == 'f') {
+        } else if (str.startsWith("f")) {
             return "Fizz";
-        }else if(str.charAt(str.length() -1)=='b') {
+        }else if(str.endsWith("b")) {
             return "Buzz";
         }else{
             return str;
